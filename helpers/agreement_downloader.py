95cf69b7638b413ff8c151937a6e976195e88f37
ZPXD <zajeciaprogramowaniaxd@gmail.com>
2022-04-24 12:16:02 +0200

    Adding helper sketch.

---
+++ helpers/agreement_downloader.py
@@ -0,0 +1,20 @@
+import requests
+import sys
+import os
+
+url = "x"
+
+def download_contract_from_url(url):
+	print(url)
+
+def format_decider():
+	pass
+
+def get_agreement_text():
+	pass
+
+
+if __name__ == "__main__":
+	if len(sys.argv) > 1:
+		url = sys.argv[1]
+	download_contract_from_url(url)
